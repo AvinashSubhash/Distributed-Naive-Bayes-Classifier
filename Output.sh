c7ffb5130a8de891286b3bcea047f70056f4e605
AvinashSubhash <avinashsubhash19@gmail.com>
2022-05-16 11:21:16 +0530

    Minor Update-2

---
+++ Output.sh
@@ -0,0 +1,12 @@
+printf "Naive Bayes ALgorithm Analysis with different Distributed Computing Modules\n\n"
+printf "********\n\n"
+printf "Method 1: Plain Naive Bayes\nMethod 2: Naive Bayes with MPI\nMethod 3: Naive Bayes with Multiprocessing and MultiThreading\n\n"
+echo "Normal Naive Bayes Program: "
+command python3 PlainNaiveBayes.py
+echo ""
+echo "Naive Bayes with MPI: "
+command mpiexec -n 4 python3 MPINaiveBayes.py
+echo ""
+echo "Naive Bayes with Multiprocessing and Multithreadiung: "
+command python3 DistributedNaiveBayes.py
+printf "\n\n"
